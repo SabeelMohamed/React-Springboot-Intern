b37e1b899ff96fb9e6312dfdae69c81e796343de
SabeelMohamed <sabeelmohamed1818@gmail.com>
2025-07-02 20:11:37 +0530

    Initial Commit

---
+++ src/main/java/org/example/Instructor.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Instructor {
+    public Instructor() {
+        System.out.println("Instructor");
+    }
+    public void abc(String name) {
+        System.out.println("Instructor " + name);
+
+    }
+}
